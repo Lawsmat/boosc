b432b2215f17ad511aa4ada01e641d89bbe22fff
MoonBarc <lawson.b.cale@gmail.com>
2020-01-12 17:22:54 -0500

    cross platform support

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,5 @@
 function edit() {
-    var cp = require("child_process");
-    cp.exec(__dirname + "/../boosc_config.txt");
+    require("open")(__dirname + "/../boosc_config.txt")
     require("./ci").prompt()
 }
 
